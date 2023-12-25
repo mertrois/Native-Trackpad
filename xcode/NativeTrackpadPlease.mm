fcaaec1bc8e5f819020381708536b63cac6bb094
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-12-25 22:33:54 +0100

    next

--- xcode/NativeTrackpadPlease.mm
+++ xcode/NativeTrackpadPlease.mm
@@ -131,26 +131,32 @@ int howWeShouldHandleEvent(NSEvent* event) {
     if (event.type != NSEventTypeScrollWheel && event.type != NSEventTypeMagnify && event.type != NSEventTypeGesture && event.type != NSEventTypeSmartMagnify) {
         return 0;
     }
-    if (event.modifierFlags != 0) {
-        return 0;
-    }
-    if (!app->activeViewport()) {
-        return 0;
-    }
     if (![event.window.title hasPrefix: @"Autodesk Fusion 360"]) {
         return 0;
     }
     
     if (event.type == NSEventTypeGesture) {
+        if (event.modifierFlags != 0 || !app->activeViewport()) {
+            return 0;
+        }
         return 1;
     }
     if (event.type == NSEventTypeScrollWheel) {
+        if (event.modifierFlags != 0 || !app->activeViewport()) {
+            return 0;
+        }
         return 2;
     }
     if (event.type == NSEventTypeMagnify) {
+        if (event.modifierFlags != 0 || !app->activeViewport()) {
+            return 0;
+        }
         return 3;
     }
     if (event.type == NSEventTypeSmartMagnify) {
+        if (event.modifierFlags != 0 || !app->activeViewport()) {
+            return 0;
+        }
         return 4;
     }
     
